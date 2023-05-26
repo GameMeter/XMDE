0d9f6a97a7a1a7159cd79c6b0cada2f06f5c5484
GameMeter <134675517+GameMeter@users.noreply.github.com>
2023-05-26 06:24:07 +0000

    Update xmonad-configure.sh

--- xmonad-configure.sh
+++ xmonad-configure.sh
@@ -1,25 +1,25 @@
 # Making the XMonad config directory and file
-mkdir .xmonad
-touch .xmonad/xmonad.hs
-cd .xmonad
+mkdir /home/$(whoami)/.xmonad
+touch /home/$(whoami)/.xmonad/xmonad.hs
+cd /home/$(whoami)/.xmonad
 # Activating a script that writes the config
-sh XMDE/xmonad-config-write.sh
+sudo sh XMDE/xmonad-config-write.sh
 # Moving the default image
 cd
-mkdir wallpaper
-mv XMDE/wallpaper/*.png wallpaper/
+mkdir /home/$(whoami)/wallpaper
+mv /home/$(whoami)/XMDE/wallpaper/*.png wallpaper/
 # User has to setup nitrogen on their own bcuz its GUI-based
 # Making dir for the xmobar config
-mkdir .config/xmobar
-touch .config/xmobar/xmobarrc
-cd .config/xmobar
+mkdir /home/$(whoami)/.config/xmobar
+touch /home/$(whoami)/.config/xmobar/xmobarrc
+cd /home/$(whoami)/.config/xmobar
 # Executing a script that writes the config
-sh xmobar-config-write.sh
+sh /home/$(whoami)/XMDE/xmobar-config-write.sh
 # Creating the picom config
 cd
-mkdir .config/picom
-touch .config/picom/picom.conf
-cd .config/picom
+mkdir /home/$(whoami)/.config/picom
+touch /home/$(whoami)/.config/picom/picom.conf
+cd /home/$(whoami)/.config/picom/
 echo "corner-radius = 10;" > picom.conf
 # Install the custom grub bootloader
 sudo sh /home/$(whoami)/XMDE/xenlism-grub-arch-1080p/install.sh
