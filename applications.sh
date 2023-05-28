18b4a199617693addc656b2ea0bc674679732878
GameMeter <134675517+GameMeter@users.noreply.github.com>
2023-05-28 14:33:41 +0000

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -5,6 +5,6 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si --noconfirm
 # Installing the packages for my build
-paru -S --noconfirm xorg xorg-xrandr xorg-xinit terminater windscribe-v2-bin lxapperance zsh thunar zsh-theme-powerlevel10k-git librewolf-bin vlc nomacs-git xmonad xmobar qt xmonad-contrib dmenu nitrogen picom-tryone-git pamixer sublime-text-4 timeshift btop neofetch sddm-theme-corners-git sddm oreo-cursors orchis-theme-git
+paru -S --noconfirm xorg rofi xorg-xrandr xorg-xinit terminater windscribe-v2-bin lxapperance zsh thunar zsh-theme-powerlevel10k-git librewolf-bin vlc nomacs-git xmonad xmobar qt xmonad-contrib dmenu nitrogen picom-tryone-git pamixer sublime-text-4 timeshift btop neofetch sddm-theme-corners-git sddm oreo-cursors orchis-theme-git
 # Returning to home directory for stage 2
 cd 
