a311009cbaba4fb1ff382e569b6f29c215f2c666
GameMeter <134675517+GameMeter@users.noreply.github.com>
2023-05-28 16:38:17 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,12 @@ sudo sh /home/$(whoami)/XMDE/applications.sh
 # Stage 2
 sh /home/$(whoami)/XMDE/xmonad-configure.sh
 # Stage 3 
-echo "You need to setup the wallpaper yourself by nitrogen
+echo "
 
-Main-packages: 
-git
-xmonad, xmobar, xmobar-contrib
-timeshift
-vlc, nomacs
-vim, codium, sublime 4
-nitrogen
-btop, neofetch 
+You will need to setup the SDDM background, XMonad background, GTK themes and cursors themes yourself
+For XMonad: Nitrogen
+GTK Themes: orchis-theme-git -> lxappearance
+cursors theme: oero-cursors-git -> lxappearance
 
+If the installation failed then, it either has bugs i don't know of, you didnt install paru or tough luck
 "
