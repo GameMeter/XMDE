c81e9ba7aa64f4602a33dbf783599f57b6b310c0
GameMeter <134675517+GameMeter@users.noreply.github.com>
2023-05-28 16:25:50 +0000

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -1,7 +1,5 @@
 cd
 # Installing the packages for my build
-paru -S orchis-theme-git
-paru -S oreo-cursors-git
 paru -S --noconfirm xorg rofi xorg-xrandr xorg-xinit terminator-git windscribe-v2-bin lxappearance zsh thunar zsh-theme-powerlevel10k-git librewolf vlc nomacs-git xmonad xmobar qt xmonad-contrib dmenu nitrogen picom-tryone-git pamixer timeshift btop neofetch sddm-theme-corners-git sddm orchis-theme-git
 # Returning to home directory for stage 2
 cd 
