e39609afcdda8d93f0b06a75e1c723e7e3d4a892
GameMeter <134675517+GameMeter@users.noreply.github.com>
2023-06-01 13:09:21 +0000

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -1,7 +1,5 @@
 cd
 # Installing the packages for my build
-# Builds that require manual intervention
-paru -S windscribe nomacs zsh-theme-powerlevel10k-git librewolf picom-tryone-git timeshift orchis-theme-git oreo-cursors sddm-theme-corners-git wireshark
 # Builds that require no manual intervention
 paru -S --noconfirm xorg rofi xorg-xrandr vlc conky-manager xorg-xinit alacritty lxappearance zsh thunar xmonad qt xmonad-contrib dmenu nitrogen pamixer btop neofetch sddm
 # Returning to home directory for stage 2
