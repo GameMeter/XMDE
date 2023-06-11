511182a29df0f438316f794bfa95e5d7e416c395
GameMeter <134675517+GameMeter@users.noreply.github.com>
2023-06-11 12:18:37 +0000

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -1,6 +1,6 @@
 cd
 # Installing the packages for my build
 # Builds that require no manual intervention
-paru -S --noconfirm xorg rofi xorg-xrandr vlc conky-manager xorg-xinit alacritty lxappearance zsh thunar xmonad qt xmonad-contrib dmenu nitrogen pamixer btop neofetch sddm
+paru -S --noconfirm xorg rofi xorg-xrandr vlc conky-manager xorg-xinit alacritty lxappearance thunderbird keepassxc zsh thunar xmonad qt xmonad-contrib dmenu nitrogen pamixer btop neofetch sddm
 # Returning to home directory for stage 2
 cd 
