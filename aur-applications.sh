de9edc4767871488350c9ad3efe3209ecd538def
GameMeter <134675517+GameMeter@users.noreply.github.com>
2023-06-11 13:56:39 +0000

    Update aur-applications.sh

--- aur-applications.sh
+++ aur-applications.sh
@@ -1,2 +1,3 @@
 # Builds that require manual intervention
 paru -S windscribe nomacs zsh-theme-powerlevel10k-git librewolf picom-tryone-git timeshift-bin orchis-theme-git sddm-theme-corners-git wireshark-git
+paru -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
